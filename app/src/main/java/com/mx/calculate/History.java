fbc8474581414f8c9a3de47cd3cfd5650aa3adfb
yaoquanmin <2515240436@qq.com>
2021-02-24 02:15:56 +0800

    0224

---
+++ app/src/main/java/com/mx/calculate/History.java
@@ -0,0 +1,28 @@
+package com.mx.calculate;
+
+
+public class History {
+    private String ques;
+    private String result;
+
+    public History(String ques, String result) {
+        this.ques = ques;
+        this.result = result;
+    }
+
+    public String getQues() {
+        return ques;
+    }
+
+    public void setQues(String ques) {
+        this.ques = ques;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
